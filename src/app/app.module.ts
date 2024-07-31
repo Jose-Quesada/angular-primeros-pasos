a4d042f0dde73167be31cf7bbf6db15b90f2a24b
Jose Quesada <jose.quesada@fernando3martos.com>
2024-07-31 19:38:07 +0200

    Fin módulo 5 Bases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
@@ -13,9 +14,10 @@ import { AppComponent } from './app.component';
 
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule,
   ],
   providers: [],
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[]= [{
+    name: 'Trunk',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+  //public onDelete = new EventEmitter<number>(); igual al anterior
+
+  onDeleteCharacter (id?: string):void{
+    if (!id) return;
+
+    this.onDelete.emit (id);
+  }
+}
